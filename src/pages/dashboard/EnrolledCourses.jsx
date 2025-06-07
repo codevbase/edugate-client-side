50a84e1ff703d051768c1e858b3ce140875b984e
lab-machine <ahasanhabib51971@gmail.com>
2025-06-07 17:01:30 +0600

    built project structure

---
+++ src/pages/dashboard/EnrolledCourses.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EnrolledCourses = () => {
+    return (
+        <div>
+            <h1>Enrolled Courses showing up</h1>
+        </div>
+    );
+};
+
+export default EnrolledCourses;
